5945b4c867b4fe3183728ea9d418c35561b0885b
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-02-20 22:05:23 +0530

    mongo query added

---
+++ backend/mongo-setup.js
@@ -0,0 +1,50 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const dbName = 'Portfolio';
+const url = 'mongodb://localhost:27017';
+function mongoSetup(collectionName) {
+    return MongoClient.connect(url).then(client => {
+        const collection = client.db(dbName).collection(collectionName);
+        return { collection, client };
+    })
+    .catch(err => console.log('mongo error : ', err));
+};
+
+function findQuery(collection, filter) {
+    return collection.find(filter).toArray();
+}
+function insertOne(collection, doc) {
+    return collection.insertOne(doc);
+}
+function findOneAndReplace(collection, filter, doc) {
+    doc['_id'] = getObjectId(doc['_id']);
+    return collection.findOneAndUpdate(filter, {
+        $set: doc
+    }).then((response) => {
+        if (!response.value) {
+            throw new Error('Data Not found');
+        }
+        return response;
+    });
+}
+
+function getObjectId(id) {
+    return ObjectId(id);
+}
+
+module.exports = {
+    mongoSetup,
+    getObjectId,
+    findQuery,
+    insertOne,
+    findOneAndReplace
+};
+
+/**
+
+const collection = db.collection(collectionName);
+collection.find().toArray((err, docs) => {
+    console.log('docs : ', docs.length);
+    client.close();
+});
+
+**/
